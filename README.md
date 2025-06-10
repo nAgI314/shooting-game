878edb64a47e30ca00622414c00742dd69c7e1dd
nAgI314 <kurosawasyunnagi@gmail.com>
2025-06-10 20:01:06 +0900

    scale and translation good

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # shooting-game
+
+#### 初めに
+` docker-compose up --build ` 
+
+#### 実行中にコードを編集した時は
+`docker-compose run rust cargo run `
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::*;
+use bevy::{color::palettes::css::YELLOW, input::{keyboard::KeyboardInput, ButtonState}, prelude::*};
+
+const WINDOW_SIZE: Vec2 = Vec2::new(640.0, 480.0);
+const GAMETITLE: &str = "shooting";
 
 #[derive(Component)]
 enum Direction {
@@ -10,20 +13,82 @@ enum Direction {
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    resolution: WINDOW_SIZE.into(),
+                    title: GAMETITLE.to_string(),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            })
+        )    
         .add_systems(Startup, setup,)
         .add_systems(Update, sprite_movement)
+        .add_systems(Update, keyboard_events)
+        .add_systems(Update,move_player)
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2d);
+    commands.spawn(
+        Camera2d::default(),
+    );
+    // 四角形
+    commands.spawn((
+        Sprite {    
+            ..Default::default()
+        },
+        Transform::from_scale(Vec3::new(100.0, 100.0, 1.0)),
+    ));
+    //黄色い点
+    // commands.spawn(
+    //     Sprite{
+    //         image: asset_server.load("bevy_bird_dark.png"),
+    //         color:YELLOW.into(),
+    //         ..default() 
+    // });
+    //回る鳥
     commands.spawn((
         Sprite::from_image(asset_server.load("bevy_bird_dark.png")),
-        Transform::from_xyz(0., 0., 0.),
+        Transform {
+            scale: Vec3{
+                x: 0.2,
+                y: 0.2,
+                z: 0.2},
+            translation: Vec3 { 
+                x: 100.0, 
+                y: 100.0, 
+                z: 1.0},
+            ..default()
+        },
+        
         Direction::Right,
     ));
-}
+    commands.spawn((
+        Sprite{
+            // image:from_image(asset_server.load("bevy_bird_dark.png")),
+            color:YELLOW.into(),
+            ..Default::default()},
+        Transform {
+            scale: Vec3{
+                x: 10.0,
+                y: 10.0,
+                z: 10.0},
+            translation: Vec3 { 
+                x: 100.0, 
+                y: 100.0, 
+                z: 1.0},
+            ..default()
+        },
+        // Direction::Right,
+    ));
+//     commands.spawn(PointLight {
+//         color:YELLOW.into(),
+//         intensity: 1000.0,
+//     ..default()
+// });
+    }
 
 fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
     for (mut logo, mut transform) in &mut sprite_position {
@@ -45,3 +110,20 @@ fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, 
         }
     }
 }
+
+fn keyboard_events(
+    mut evr_kbd: EventReader<KeyboardInput>,
+) {
+    for ev in evr_kbd.read() {
+        match ev.state {
+            ButtonState::Pressed => {
+                println!("Key press: {:?} ({:?})", ev.key_code, ev.logical_key);
+            }
+            ButtonState::Released => {
+                println!("Key release: {:?} ({:?})", ev.key_code, ev.logical_key);
+            }
+        }
+    }
+}
+
+fn move_player(){}
